896f9df8777ad748c5f6fc86350c6e0a93ef976b
Arnab S <arnabsengupta104@gmail.com>
2024-08-25 11:00:45 +0530

    auth

---
+++ main/app/pharmacy/auth/signup/route.js
@@ -0,0 +1,55 @@
+import { ProviderSchema, RatingSchema } from "@/app/_models/schema";
+import connectToDatabase from "@/app/_middleware/mongodb";
+import mongoose from "mongoose";
+import jwt from 'jsonwebtoken';
+import { hash } from "bcrypt";
+import { NextResponse } from 'next/server';
+
+const secretKey = process.env.SECRET_KEY;
+
+async function posthandler(req) {
+    await connectToDatabase();
+    let body;
+    try {
+        body = await req.json();
+    } catch (error) {
+        console.log("Error parsing request body", error);
+        return NextResponse.json({ error: "Invalid request body" }, { status: 400 });
+    }
+
+    const { name, email, password, type, latitude, longitude } = body;
+
+    if (!name || !email || !password || !type || !latitude || !longitude) {
+        return NextResponse.json({ error: "Missing required fields" }, { status: 400 });
+    }
+
+    const Provider = mongoose.models.Provider || mongoose.model('Provider', ProviderSchema);
+    const Rating = mongoose.models.Rating || mongoose.model('Rating', RatingSchema);
+
+    const hashedPassword = await hash(password, 10);
+
+    try {
+        const existingProvider = await Provider.findOne({ email });
+        if (existingProvider) {
+            return NextResponse.json({ error: "Provider already exists" }, { status: 400 });
+        }
+
+        const provider = new Provider({ name, email, password: hashedPassword, type, latitude, longitude });
+        await provider.save();
+
+        const rating = new Rating({ providerId: provider._id, rating: 0 });
+        await rating.save();
+
+        const token = jwt.sign({ providerId: provider._id }, secretKey, { expiresIn: "30d" });
+
+        return NextResponse.json({ token }, { status: 200 });
+    }
+    catch (error) {
+        console.error("Error saving provider", error);
+        return NextResponse.json({ error: "Failed to register provider" }, { status: 500 });
+    }
+}
+
+export {
+    posthandler as POST
+}
